cc4c6603bad9a8c958b9bb40e9f7218794e9e164
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2024-07-09 13:34:00 -0400

    minor change

--- src/components/components.js
+++ src/components/components.js
@@ -3,6 +3,6 @@ import Logo from "./Logo/Logo.jsx";
 import Button from "./Button/Button.jsx";
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
-import SignIn from "./SignIn/SignIn.jsx";
+import SignIn from "./SignIn/Signin.jsx";
 import FileInputButton from "./FileInputButton/FileInputButton.jsx";
 export { Logo, Button, Layout, Header, Footer, SignIn, FileInputButton } ;
