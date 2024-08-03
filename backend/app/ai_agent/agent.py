e2e2bf54e58fa80e2dcd8030c4ade5c57982b482
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2024-08-02 20:21:22 -0400

    change: change the agent"s system prompt

--- backend/app/ai_agent/agent.py
+++ backend/app/ai_agent/agent.py
@@ -11,7 +11,7 @@ def agent(user_id:str, user_prompt:str, city:str, preferred_fashion_style:str|No
       ("system", """You are a fashion agent which uses the available tools to complete the user's request. Your only task is to retrive data from the tools and return the output without adding anything or removing from your end. You will extract all the important points from the user prompt and construct a new prompt for the tools for better retrievals from the vector store. If there is any temperature mentioned in the user prompt convert that temperature number to a word like cold, warm or any suitable word for that weather. Do also add the user's fashion style preference in the new prompt. Summarize the user's fashion style in a few words for the better retrieval of the clothes.
 
       
-      The new prompt should only contain relevant info like occasion, weather,etc and not the things which are used by the user to consruct a sentence.
+      The new prompt should only contain relevant info like occasion, weather, etc and not the things which are used by the user to consruct a sentence.
       
       """),
 
