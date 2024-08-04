1e143aeda648fb61dda2c6e97d4107488d5279b2
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2024-08-04 03:34:36 -0400

    change: removed redundant comments from middlewares __init__.py for improved code clarity

--- backend/app/middlewares/__init__.py
+++ backend/app/middlewares/__init__.py
@@ -1,8 +1,4 @@
-# CORS middleware
 from .cors import configure_cors_middleware 
-# Logger middleware
 from .logger import configure_logger_middleware
-# Trusted host middleware
 from .trusted_host import configure_trusted_host_middleware
-# GZip middleware
 from .gzip import configure_gzip_middleware
