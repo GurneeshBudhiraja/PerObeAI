5851cb95c0312fe899897e1917f118eff1e127eb
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2024-08-04 04:27:01 -0400

    change the recommend_outfit model prompt

--- backend/app/models/recommend_outfit.py
+++ backend/app/models/recommend_outfit.py
@@ -2,17 +2,13 @@ from pydantic import BaseModel, Field
 
 class RecommendOutfit(BaseModel):
   description: str = Field(None, title="The description of the outfit", description="""
-      You will recommend an outfit based on the cloth urls provided to you. You will account the weather condition, user prompt, preferred style and accessibility issues of the user. You will then select the clothes which when paired together makes a great outfit. You will then provide a brief description of the outfit selected such that when paired together(one from the clothing type 'upperwear' and other of type 'lowerwear'), they form a great outfit considering the user's prompt, preferred style and the weather. 
+      You will recommend an outfit based on the cloth urls provided to you. You will account the weather condition, user prompt, preferred style. You will then select the clothes which when paired together makes a great outfit. You will then provide a brief description of the outfit selected such that when paired together (atleast one from the clothing type 'upperwear' and other of type 'lowerwear'), they form a great outfit. Do not discriminate while selecting the outfit for the user irrespective of any visual impairments or not.
       
-      Make sure the description describes the clothing items in such a way that the user is able to get what has been referred to without having to click on the URL.
+      If the user has no sort of visual impairements i.e., has normal vision you will explain the image in a normal like pointing to a specific thing that will help the user know what has been referred to. Make sure the description is no longer than 3 lines and should be helpful, easy to understand,and can include a positive/friendly message that brings the smile to the user (and can use emojis wherever required). 
       
-      If the user has no sort of visual impairements you will explain the image in a normal like pointing to a specific thing that will help the user know what has been referred to. Make sure the description is no longer than 2 lines and should be more on casual side only if the customer does not provide any sort of accessibility issue. 
-      
-      If the user has visual impairment of blindness, the description should be detailed enough to help the person identify the clothing items by touching it or feeling it or through whatever means the description would be helpful for the blind person. 
+      If the user is blind : Generate a highly detailed clothing description tailored for a visually impaired individual. Prioritize tactile and sensory elements over visual aspects. Describe clothing items using terms related to texture, material, fit, and overall style. Avoid color-centric descriptions unless essential for understanding the item's overall appearance. Ensure the description is positive, empathetic, and easy to comprehend. The goal is to create a vivid mental image through touch and imagination.
 
-      If the user has visual impairment of color blindness, give the description in such a way that the color blind person of that specific type is able to understand the color of the clothing item. Make sure the description is also detailed enough to help the person identify the clothing items by seeing it as you will be mentioning the colors that are seen by that color blind person. 
+      If the user is color blind, there is no limit on the lenght of the description. The description is in such a way that the color blind person able to understand the color of the clothing item. Make sure the description helps the user identify the clothing items by seeing it as you will be mentioning the colors in the same way those colors are perceived by the color blind user. Make sure the description should be helpful, empathetic, easy to understand and can include a positive/friendly message that brings the smile to the user (and can use emojis wherever required).  
 
-      Also, keep the description short while helping the user. At last add a positive note to the description to make the user feel good about the outfit selected for them.
-
-      At last, if you fail to find any good recommendations, return a positive message to the user mentioning that you are unable to find any good recommendations for the user. In that case you can give some general tips to the user to help them find the right outfit for them.
+      At last, if you fail to find any good recommendations, return a simple message to the user mentioning about that and can give some tips from your end.). 
     """)
