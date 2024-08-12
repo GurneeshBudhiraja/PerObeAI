2810e239894916ff00a1f07b0462472307e4eca0
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2024-08-12 07:54:29 -0400

    fix the vercel error

--- frontend/src/components/components.js
+++ frontend/src/components/components.js
@@ -1,4 +1,4 @@
-import Layout from "./layout/Layout.jsx"
+import Layout from "./layout/Layout.jsx";
 import Header from "./header/Header.jsx";
 import Footer from "./footer/Footer.jsx";
 import ReactHelmet from "./reactHelmet/ReactHelmet.jsx";
