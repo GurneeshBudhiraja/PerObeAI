52e10bc7dddc1220f6c88176baff41c6cb6215e7
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2024-08-13 23:53:20 -0400

    edit the allowed host

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,5 +1,5 @@
 # List of allowed origins for the CORS middleware
-ALLOWED_ORIGINS = ["https://perobeai.vercel.app/"]
+ALLOWED_ORIGINS = ["https://perobeai.vercel.app"]
 
 # List of allowed hosts for the FastAPI trusted host middleware
 ALLOWED_HOSTS = ["perobeai.vercel.app/"]
